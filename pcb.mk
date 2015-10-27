53b1624aff365a992085766e460dadcd049b7b62
Keith Packard <keithp@keithp.com>
2015-10-27 21:13:48 +0900

    Adjust oshpark targets. Add seeed targets
    Try to pull correct filenames for the oshpark target.
    Add seeed target for both PCB and BOM.
    
    Signed-off-by: Keith Packard <keithp@keithp.com>

--- pcb.mk
+++ pcb.mk
@@ -28,17 +28,20 @@ partslist.csv:	$(SCHEMATICS) Makefile $(AM)/preferred-parts
 	gnetlist -L $(SCHEME) -g partslistgag -o $(PROJECT).csvtmp $(SCHEMATICS)
 	(head -n1 $(PROJECT).csvtmp; tail -n+2 $(PROJECT).csvtmp | sort -t \, -k 8 | awk -f $(AM)/bin/fillpartscsv ) > $@ && rm -f $(PROJECT).csvtmp
 
-partslist.dk: $(SCHEMATICS) Makefile $(AM)/preferred-parts partslist.csv
+partslist.dk: partslist.csv
 	$(AM)/bin/partslist-vendor --vendor digikey partslist.csv > $@
 
-partslist-check.dk: $(SCHEMATICS) Makefile $(AM)/preferred-parts partslist.csv
+partslist-check.dk: partslist.csv
 	$(AM)/bin/partslist-vendor --vendor digikey --mfg partslist.csv > $@
 
-partslist.mouser: $(SCHEMATICS) Makefile $(SCHEME)/gnet-partslist-bom.scm
+partslist.mouser: partslist.csv
 	$(AM)/bin/partslist-vendor --vendor mouser partslist.csv > $@
 
-partslist.other: $(SCHEMATICS) Makefile $(SCHEME)/gnet-partslist-bom.scm
+partslist.other: partslist.csv
 	$(AM)/bin/partslist-vendor --not-vendor digikey,mouser partslist.csv > $@
+
+$(PROJECT)-seeed.csv: partslist.csv
+	$(AM)/bin/partslist-vendor --vendor seeed partslist.csv > $@
 
 pcb:	$(SCHEMATICS) project Makefile
 	gsch2pcb project
@@ -48,22 +51,73 @@ $(PROJECT).xy:	$(PROJECT).pcb
 
 $(PROJECT).bottom.gbr:	$(PROJECT).pcb
 	pcb -x gerber $(PROJECT).pcb
+	@case "$(SILK)" in \
+	none) 	rm -f $(PROJECT).topsilk.gbr $(PROJECT).bottom.gbr; \
+		;; \
+	top) 	rm -f $(PROJECT).bottomsilk.gbr; \
+		;; \
+	bottom) rm -f $(PROJECT).topsilk.gbr; \
+		;; \
+	both) \
+		;; \
+	*) 	echo "Invalid silk $(SILK)"; exit 1; \
+		;; \
+	esac
 
-zip:	$(PROJECT).bottom.gbr $(PROJECT).bottommask.gbr $(PROJECT).fab.gbr $(PROJECT).top.gbr $(PROJECT).topmask.gbr $(PROJECT).toppaste.gbr $(PROJECT).group2.gbr $(PROJECT).group3.gbr $(PROJECT).plated-drill.cnc $(PROJECT).xy  Makefile # $(PROJECT).xls
-	rm -f $(PROJECT).topsilk.gbr
+zip: $(PROJECT).zip
+
+$(PROJECT).zip: $(PROJECT).bottom.gbr Makefile
 	zip $(PROJECT).zip $(PROJECT).*.gbr $(PROJECT).*.cnc $(PROJECT).xy # $(PROJECT).xls
 
-oshpark: $(PROJECT).bottom.gbr $(PROJECT).bottommask.gbr $(PROJECT).top.gbr $(PROJECT).topmask.gbr $(PROJECT).plated-drill.cnc
+oshpark: $(PROJECT)-oshpark.zip
+
+$(PROJECT)-oshpark.zip: $(PROJECT).bottom.gbr
 	cp $(PROJECT).bottom.gbr bottom\ layer.ger
 	cp $(PROJECT).bottommask.gbr bottom\ solder\ mask.ger
-	cp $(PROJECT).bottomsilk.gbr bottom\ silk\ screen.ger
+	if [ -f $(PROJECT).bottomsilk.gbr ]; then \
+		cp $(PROJECT).bottomsilk.gbr bottom\ silk\ screen.ger; \
+	fi
+	if [ -f $(PROJECT).topsilk.gbr ]; then \
+		cp $(PROJECT).topsilk.gbr top\ silk\ screen.ger; \
+	fi
 	cp $(PROJECT).outline.gbr board\ outline.ger
 	cp $(PROJECT).top.gbr top\ layer.ger
 	cp $(PROJECT).topmask.gbr top\ solder\ mask.ger
 	cp $(PROJECT).plated-drill.cnc drills.xln
-	cp $(PROJECT).group2.gbr internal\ plane\ 1.ger
-	cp $(PROJECT).group3.gbr internal\ plane\ 2.ger
+	if [ -f $(PROJECT).group1.gbr -a -f $(PROJECT).group2.gbr ]; then \
+		cp $(PROJECT).group1.gbr internal\ plane\ 1.ger; \
+		cp $(PROJECT).group2.gbr internal\ plane\ 2.ger; \
+	elif [ -f $(PROJECT).group2.gbr -a -f $(PROJECT).group3.gbr ]; then \
+		cp $(PROJECT).group2.gbr internal\ plane\ 1.ger; \
+		cp $(PROJECT).group3.gbr internal\ plane\ 2.ger; \
+	fi
 	zip $(PROJECT)-oshpark.zip *.ger *.xln
+
+seeed: $(PROJECT)-seeed.zip $(PROJECT)-seeed.csv
+
+$(PROJECT)-seeed.zip: $(PROJECT).bottom.gbr
+	cp $(PROJECT).bottom.gbr $(PROJECT).gbl
+	cp $(PROJECT).bottommask.gbr $(PROJECT).gbs
+	if [ -f $(PROJECT).bottomsilk.gbr ]; then \
+		cp $(PROJECT).bottomsilk.gbr $(PROJECT).gbo; \
+	fi
+	if [ -f $(PROJECT).topsilk.gbr ]; then \
+		cp $(PROJECT).topsilk.gbr $(PROJECT).gto; \
+	fi
+	cp $(PROJECT).outline.gbr $(PROJECT).gml
+	cp $(PROJECT).top.gbr $(PROJECT).gtl
+	cp $(PROJECT).topmask.gbr $(PROJECT).gts
+	cp $(PROJECT).plated-drill.cnc $(PROJECT).txt
+	if [ -f $(PROJECT).group1.gbr -a -f $(PROJECT).group2.gbr ]; then \
+		cp $(PROJECT).group1.gbr $(PROJECT).gl2; \
+		cp $(PROJECT).group2.gbr $(PROJECT).gl3; \
+	elif [ -f $(PROJECT).group2.gbr -a -f $(PROJECT).group3.gbr ]; then \
+		cp $(PROJECT).group2.gbr $(PROJECT).gl2; \
+		cp $(PROJECT).group3.gbr $(PROJECT).gl3; \
+	fi
+	zip $(PROJECT)-seeed.zip $(PROJECT).gbl $(PROJECT).gbs $(PROJECT).gbo \
+		$(PROJECT).gto $(PROJECT).gml $(PROJECT).gtl $(PROJECT).txt \
+		$(PROJECT).gl2 $(PROJECT).gl3
 
 stencil:	$(PROJECT).bottom.gbr $(PROJECT).toppaste.gbr $(PROJECT).outline.gbr
 	zip $(PROJECT)-stencil.zip $(PROJECT).toppaste.gbr $(PROJECT).outline.gbr
